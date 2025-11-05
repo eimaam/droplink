e3b282832a5ad74e7e55c3c458c8d9f7d0705f0a
Imam Dahir <imamddahir@gmail.com>
2025-11-05 21:17:18 +0100

    feat: Add user types and enums for role, provider, and status management

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, '../shared'),
+    },
   },
 });
---
+++ shared/types/user.types.ts
@@ -0,0 +1,19 @@
+export enum UserRoleEnum  {
+    USER = 'user',
+    SUPER_ADMIN = 'super_admin',
+    ADMIN = 'admin',
+}
+
+export enum AuthProvider {
+    LOCAL = 'local',
+    GOOGLE = 'google',
+    GITHUB = 'github',
+}
+
+export enum UserStatus {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+    BANNED = 'banned',
+    DELETED = 'deleted',
+}
+
